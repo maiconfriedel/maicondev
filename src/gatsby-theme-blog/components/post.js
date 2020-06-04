7935105b7dadc8bc30aa000b831c3dd6f4de4152
Maicon Gabriel Friedel <maicon.friedel@kyly.com.br>
2020-06-04 11:29:40 -0300

    Finalizado primeiro post e adicionado comentários com Disqus

--- src/gatsby-theme-blog/components/post.js
+++ src/gatsby-theme-blog/components/post.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { DiscussionEmbed } from "disqus-react"
 
 import { MDXRenderer } from "gatsby-plugin-mdx"
 
@@ -19,27 +20,35 @@ const Post = ({
   location,
   previous,
   next,
-}) => (
-  <Layout location={location} title={title}>
-    <SEO
-      title={post.title}
-      description={post.excerpt}
-      imageSource={
-        post.socialImage
-          ? post.socialImage?.childImageSharp?.fluid.src
-          : post.image?.childImageSharp?.fluid.src
-      }
-      keywords={post.keywords}
-      imageAlt={post.imageAlt}
-    />
-    <main>
-      <PostHero post={post} />
-      <PostTitle>{post.title}</PostTitle>
-      <PostDate>{post.date}</PostDate>
-      <MDXRenderer>{post.body}</MDXRenderer>
-    </main>
-    <PostFooter {...{ previous, next }} />
-  </Layout>
-)
+}) => {
+  const disqusConfig = {
+    shortname: process.env.GATSBY_DISQUS_NAME,
+    config: { identifier: title },
+  }
+
+  return (
+    <Layout location={location} title={title}>
+      <SEO
+        title={post.title}
+        description={post.excerpt}
+        imageSource={
+          post.socialImage
+            ? post.socialImage?.childImageSharp?.fluid.src
+            : post.image?.childImageSharp?.fluid.src
+        }
+        keywords={post.keywords}
+        imageAlt={post.imageAlt}
+      />
+      <main>
+        <PostHero post={post} />
+        <PostTitle>{post.title}</PostTitle>
+        <PostDate>{post.date}</PostDate>
+        <MDXRenderer>{post.body}</MDXRenderer>
+        <DiscussionEmbed {...disqusConfig} />
+      </main>
+      <PostFooter {...{ previous, next }} />
+    </Layout>
+  )
+}
 
 export default Post
